286a12895b7c1bc64c13e8e8d875e81f464e9313
treindle <treindle@gmail.com>
2016-10-18 15:59:11 -0700

    Initialization

---
+++ public/js/maincomponent.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  var $landing_header = $('#landing_header'),
+      $landing_subheader = $('#landing_subheader'),
+      $products_button = $('#products_button'),
+      $values_button = $('#values_button');
+
+  $.ajax({
+    url: 'content.json',
+    type: 'GET',
+    success: function(response) {
+      object = response;
+      console.log(object)
+      main_info = object.Main_Info;
+      main_info_keys = Object.keys(main_info);
+      main_info_cat = main_info[main_info_keys];
+      for (i in object.Main_Info) {
+        // console.log(object[i])
+      }
+      for (i in main_info[main_info_keys]) {
+      }
+        console.log(main_info_cat)
+
+      // landing top nav population
+      $landing_header.empty().append(main_info_cat.Header)
+      $landing_subheader.empty().append(main_info_cat.Subheader)
+
+    },
+      error: function(jqXHR, textStatus, errorThrown) {
+      console.log('No JSON detected. Check that syntax.');
+    }
+  });
+});
+  
+
+
